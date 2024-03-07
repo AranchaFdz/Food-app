320a8fa312fc7229bfa8be7b674f71dfc67bf5f1
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2024-03-07 14:43:58 +0100

    Completed all models and starting to configurate the security

---
+++ online_food_ordering/src/test/java/com/origen/online_food_ordering/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origen.online_food_ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
