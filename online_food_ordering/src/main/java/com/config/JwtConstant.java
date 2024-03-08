3482c37e7e87338f8e42fbd32c9ffe7dcece516d
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2024-03-08 14:48:39 +0100

    Autorization JWT

---
+++ online_food_ordering/src/main/java/com/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.config;
+
+public class JwtConstant {
+  public static final String SECRET_KEY="vnrñeguhñesiur5hñneisubviuebsvñjbnñoieruhaobevchjrgv";
+  public static final String JWT_HEADER="Aurthorization";
+  
+}
